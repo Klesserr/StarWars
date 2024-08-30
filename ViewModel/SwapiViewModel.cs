72a613f28ef70ff8a4380f77e869011d463af895
Jesus <jesusvico203@gmail.com>
2024-08-30 10:46:19 +0200

    Crear una nueva clase ViewModel para el Swapi

---
+++ ViewModel/SwapiViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StarWars.ViewModel
+{
+	public class SwapiViewModel
+	{
+		public string NamePeople { get; set; }
+		public string HairColor { get; set; }
+		public string HomeWorld {  get; set; }
+		public string NamePlanet { get; set; }
+		public string TerrainPlanet { get; set; }
+	}
+}
