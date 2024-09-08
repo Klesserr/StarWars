ed4f20e30514a52a73e991dfad8de0391484a110
Jesus <jesusvico203@gmail.com>
2024-09-08 13:37:28 +0200

    Creación de métodos genericos y parte del genericView

--- Controllers/SwapiController.cs
+++ Controllers/SwapiController.cs
@@ -8,7 +8,9 @@ using StarWars.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
+using System.Net.WebSockets;
 using System.Security.Policy;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace StarWars.Controllers
 {
@@ -19,45 +21,58 @@ namespace StarWars.Controllers
 		{
 			_client = client;
 		}
+		public const string urlPeople = "https://swapi.dev/api/people";
+		public const string urlVehicle = "https://swapi.dev/api/vehicles";
+		public const string urlStarship = "https://swapi.dev/api/starships";
+		public const string urlPlanet = "https://swapi.dev/api/planets";
+		public const string urlFilm = "https://swapi.dev/api/films";
+		public ActionResult InitialPage()
+		{
+			SwapiViewModel swapiVM = new SwapiViewModel();
+			swapiVM.ListCharacterImageJson = GetUrlImagesJsonCharacters();
+			return View(swapiVM);
+		}
+		//public List<VehiclesImageJson> GetUrlImagesVehiclesJsonCharacters()
+		//{
+		//	List<VehiclesImageJson> vehiclesImagesJson = new List<VehiclesImageJson>();
+		//	string urlJson = "wwwroot/archivosJSON/character-image.json";
+		//	string fileJson = System.IO.File.ReadAllText(urlJson);
+		//	SwapiImageJson swapiImage = new SwapiImageJson();
+		//	swapiImage = JsonConvert.DeserializeObject<SwapiImageJson>(fileJson);
+		//	foreach(var i in swapiImage.ResultadoVehicles)
+		//	{
+		//		vehiclesImagesJson.Add(i);
+		//	}
+		//	return vehiclesImagesJson;
+		//}
 		public async Task<ActionResult> GetAllCharactersAPI()
 		//Se hace Task<List<Character>> Si queremos que devuelva un TaskList.Para que sea una lista cuando llamemos al método pondremos await delante.
 		{
 			List<Character> listCharacters = new List<Character>();
 			string url = $"https://swapi.dev/api/people/";
 			_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			do
+
+
+			var httpResponse = await _client.GetAsync(url);
+			if (httpResponse.IsSuccessStatusCode)
 			{
+				var responseContent = await httpResponse.Content.ReadAsStringAsync();
+				SwapiCharacter swapi = JsonConvert.DeserializeObject<SwapiCharacter>(responseContent);
 
-				var httpResponse = await _client.GetAsync(url);
-				if (httpResponse.IsSuccessStatusCode)
+				foreach (var c in swapi.Results)
 				{
-					var responseContent = await httpResponse.Content.ReadAsStringAsync();
-					Swapi swapi = JsonConvert.DeserializeObject<Swapi>(responseContent);
-
-					foreach (var c in swapi.Results)
-					{
-						listCharacters.Add(c);
-					}
-
-					string urlNext = swapi.Next;
-					if (urlNext != null)
-					{
-						url = urlNext;
-					}
-					else
-					{
-						break;
-					}
-				}
-				else
-				{
-					break;
+					listCharacters.Add(c);
 				}
 
-			} while (url != null);
+				string urlNext = swapi.Next;
+				if (urlNext != null)
+				{
+					url = urlNext;
+				}
 
+			}
+			//List<CharacterImageJson> images = new List<CharacterImageJson>();
 			return View(listCharacters);
-
 		}
 		//Realizar algo con ambos métodos (más adelante) ya que son iguales,solo cambian el valor que retornan.
 		public async Task<List<Character>> GetListAllCharactersAPI()
@@ -74,7 +89,7 @@ namespace StarWars.Controllers
 				if (httpResponse.IsSuccessStatusCode)
 				{
 					var responseContent = await httpResponse.Content.ReadAsStringAsync();
-					Swapi swapi = JsonConvert.DeserializeObject<Swapi>(responseContent);
+					SwapiCharacter swapi = JsonConvert.DeserializeObject<SwapiCharacter>(responseContent);
 					foreach (var c in swapi.Results)
 					{
 						listCharacters.Add(c);
@@ -96,6 +111,7 @@ namespace StarWars.Controllers
 				}
 
 			} while (url != null);
+
 
 			return (listCharacters);
 		}
@@ -143,7 +159,11 @@ namespace StarWars.Controllers
 			return listFilmsCharacter;
 		}
 
-
+		public async Task<ActionResult> GetFilmsAPI(List<string> url_films)
+		{
+			var query = await GetFilmsCharacterAPI(url_films);
+			return View(query);
+		}
 		public async Task<List<StarshipAPI>> GetStarshipsCharacterAPI(List<string> url_starships)
 		{
 			List<StarshipAPI> listStarship = new List<StarshipAPI>();
@@ -283,10 +303,9 @@ namespace StarWars.Controllers
 
 		public async Task<ActionResult> SearchCharacterByName(string name)
 		{
-			Swapi swapi = new Swapi();
+			SwapiCharacter swapi = new SwapiCharacter();
 			swapi.Results = await GetListAllCharactersAPI(); //Todo el listado de personas que contiene la API
 			var query = swapi.Results.Where(x => x.Name == name).ToList();
-
 
 			SwapiViewModel swapiVM = new SwapiViewModel();
 
@@ -309,6 +328,10 @@ namespace StarWars.Controllers
 				swapiVM.ListStarship = await GetStarshipsCharacterAPI(item.Starships);
 				//Propiedades de la clase Film:
 				swapiVM.ListFilms = await GetFilmsCharacterAPI(item.Films);
+				foreach (var p in swapiVM.ListFilms)
+				{
+					swapiVM.TitleFilm = p.Title;
+				}
 				//Propiedades de la clase Vehicle:
 				swapiVM.ListVehicle = await GetVehiclesCharacterAPI(item.Vehicles);
 			}
@@ -329,7 +352,7 @@ namespace StarWars.Controllers
 				}
 			}*/
 		}
-		public async Task<List<T>> GetInformation<T> (string url, List<string> urlList) where T : ISwapiHasUrl //(DONDE el generico implemente la interfaz ISwapiHasUrl)
+		public async Task<List<T>> GetInformation<T>(string url, List<string> urlList) where T : ISwapiHasUrl //(DONDE el generico implemente la interfaz ISwapiHasUrl)
 			/* where T:ISwapiHasUrl -> nos dice que el tipo T(Genérico) debe implementar esa interfaz.
 				Además ahora sabemos que cualquier tipo T tiene la propiedad Url que es la que esta definida en dicha interfaz, Ya que 
 				cualquier objeto de tipo T tendrá dicha propiedad.*/
@@ -340,24 +363,28 @@ namespace StarWars.Controllers
 			{
 				return result;
 			}
-			if (!_client.DefaultRequestHeaders.Accept.Contains(new MediaTypeWithQualityHeaderValue("application/json")))
-			{
-				_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			}
+			getAllInformationAPI = await GenericInfo(getAllInformationAPI, url);
+
+			result = AddEspecificInfo(urlList, getAllInformationAPI, result);
+
+
+			return result;
+		}
+		public async Task<List<T>> GenericInfo<T>(List<T> listGeneric, string url)
+		{
+			_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			do
 			{
 				var httpResponse = await _client.GetAsync(url);
 				if (httpResponse.IsSuccessStatusCode)
 				{
-					var responseContent = await httpResponse.Content.ReadAsStringAsync();
-					SwapiGeneric<T> response = JsonConvert.DeserializeObject<SwapiGeneric<T>>(responseContent);
-
-					foreach (var item in response.Results)
+					var contestRespone = await httpResponse.Content.ReadAsStringAsync();
+					SwapiGeneric<T> swapi = JsonConvert.DeserializeObject<SwapiGeneric<T>>(contestRespone);
+					foreach (var item in swapi.Results)
 					{
-						getAllInformationAPI.Add(item);
+						listGeneric.Add(item);
 					}
-
-					string urlNext = response.Next;
+					string urlNext = swapi.Next;
 					if (urlNext != null)
 					{
 						url = urlNext;
@@ -367,15 +394,14 @@ namespace StarWars.Controllers
 						url = null;
 					}
 				}
-
-
 			} while (url != null);
-			
-			
-
+			return listGeneric;
+		}
+		public List<T> AddEspecificInfo<T>(List<string> urlList, List<T> genericList, List<T> result) where T : ISwapiHasUrl
+		{
 			foreach (var s in urlList)
 			{
-				var query = getAllInformationAPI.FirstOrDefault(p=>p.Url == s);
+				var query = genericList.FirstOrDefault(p => p.Url == s);
 				if (query != null)
 				{
 					result.Add(query);
@@ -386,6 +412,60 @@ namespace StarWars.Controllers
 				}
 			}
 			return result;
+		}
+		public async Task<ActionResult> GetGenericView(string name, string image)
+		{
+
+			SwapiCharacter swapi = new SwapiCharacter();
+			swapi.Results = await GetListAllCharactersAPI();
+			var query = swapi.Results.Where(x => x.Name == name).ToList();
+			List<CharacterImageJson> characterImageJsons = new List<CharacterImageJson>();
+			SwapiViewModel swapiVM = new SwapiViewModel();
+			characterImageJsons = GetUrlImagesJsonCharacters();
+			swapiVM.ListCharacterImageJson = characterImageJsons;
+
+			foreach (var v in query)
+			{
+				var getPlanet = await GetPlanetAPI(v.HomeWorld);
+				swapiVM.NameCharacter = v.Name;
+				swapiVM.GenderCharacter = v.Gender;
+				swapiVM.HeightCharacter = v.Height;
+				swapiVM.MassCharacter = v.Mass;
+				swapiVM.SkinColorCharacter = v.Skin_Color;
+				swapiVM.HairColorCharacter = v.Hair_Color;
+				//
+				swapiVM.NamePlanet = getPlanet.Name;
+				swapiVM.TerrainPlanet = getPlanet.Terrain;
+				swapiVM.ClimatePlanet = getPlanet.Climate;
+				swapiVM.GravityPlanet = getPlanet.Gravity;
+				//
+				swapiVM.ListStarship = await GetInformation<StarshipAPI>(urlStarship, v.Starships);
+				swapiVM.ListFilms = await GetInformation<Film>(urlFilm, v.Films);
+				swapiVM.ListVehicle = await GetInformation<Vehicle>(urlVehicle, v.Vehicles);
+				//
+				swapiVM.NameJson = name;
+				swapiVM.ImageUrl = image;
+				//
+			}
+			
+			return View(swapiVM);
+		}
+
+
+		public List<CharacterImageJson> GetUrlImagesJsonCharacters()
+		{
+			List<CharacterImageJson> charactersImageJson = new List<CharacterImageJson>();
+
+			SwapiImageJson swapiCharacterImageJson = new SwapiImageJson();
+			string urlJson = "wwwroot/archivosJSON/character-image.json";
+			string fileJson = System.IO.File.ReadAllText(urlJson);
+			swapiCharacterImageJson = JsonConvert.DeserializeObject<SwapiImageJson>(fileJson);
+
+			foreach (var i in swapiCharacterImageJson.Resultado)
+			{
+				charactersImageJson.Add(i);
+			}
+			return charactersImageJson;
 		}
 	}
 }
